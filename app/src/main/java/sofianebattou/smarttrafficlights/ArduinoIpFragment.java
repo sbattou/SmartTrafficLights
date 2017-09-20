060351ffed7f9ca2d3bf65ae4541cb33edb91d5f
emmanuelegla <eglaemmanuel1@gmail.com>
2017-09-20 14:41:09 -0400

    Added a dialog to input the IP address of the Arduinos and connect to the intersections

---
+++ app/src/main/java/sofianebattou/smarttrafficlights/ArduinoIpFragment.java
@@ -0,0 +1,8 @@
+package sofianebattou.smarttrafficlights;
+
+/**
+ * Created by Emmanuel on 2017-09-19.
+ */
+
+public class ArduinoIpFragment {
+}
---
+++ app/src/main/java/sofianebattou/smarttrafficlights/CustomEditText.java
@@ -0,0 +1,10 @@
+package sofianebattou.smarttrafficlights;
+
+import android.widget.EditText;
+
+/**
+ * Created by Emmanuel on 2017-09-19.
+ */
+
+public class CustomEditText extends EditText {
+}
